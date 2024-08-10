244b0d66c5dadef0bc5b8bbd37cb086011c2f5db
armaghanasghar <armaghanwa@gmail.com>
2024-08-09 20:20:33 -0700

    Initial Commit

---
+++ MapKitSwiftUI/MapKitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitSwiftUIApp.swift
+//  MapKitSwiftUI
+//
+//  Created by Armaghan Asghar on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
