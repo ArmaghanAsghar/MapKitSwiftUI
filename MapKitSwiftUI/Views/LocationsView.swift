778c0a2152b6d5fb7dcf35c708f7a771d4f6311f
armaghanasghar <armaghanwa@gmail.com>
2024-08-10 17:29:33 -0700

    added a pin to the map, and made the pin selectable using the onTap gesture

--- MapKitSwiftUI/Views/LocationsView.swift
+++ MapKitSwiftUI/Views/LocationsView.swift
@@ -14,33 +14,13 @@ struct LocationsView: View {
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $vm.mapRegion)
+            mapLayer
                 .ignoresSafeArea()
-            
             VStack(spacing: 0) {
-                
                 header
                     .padding()
                 Spacer()
-                
-                ZStack {
-                    ForEach(vm.locations) { location in
-                        if vm.mapLocation == location {
-                            LocationPreviewView(location: location)
-                                .shadow(
-                                    color: Color.black.opacity(0.3),
-                                    radius: 20)
-                                .padding()
-                                .transition(.asymmetric(
-                                    insertion: .move(edge: .trailing),
-                                    removal: .move(edge: .leading)))
-                                .environmentObject(vm)
-                        }
-                    }
-                   
-                }
-              
-                
+                locationPreviewStack
             }
         }
     }
@@ -50,7 +30,6 @@ struct LocationsView: View {
     LocationsView()
         .environmentObject(LocationsViewModel())
 }
-
 
 extension LocationsView {
     
@@ -74,10 +53,6 @@ extension LocationsView {
                     }
             })
             
-            
-            
-            
-            
             if vm.showLocationsList {
                 LocationsListView()
             }
@@ -86,5 +61,38 @@ extension LocationsView {
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 15)
         
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius:10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(
+                            color: Color.black.opacity(0.3),
+                            radius: 20)
+                        .padding()
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading)))
+                        .environmentObject(vm)
+                }
+            }
+        }
     }
 }
